19ee5ae266494ff652943f8ebdc41c2aebb3b6ee
PenelopeJones <pj321@cam.ac.uk>
2020-11-23 23:58:55 +0000

    Adding sys

--- experiments/biologic_tests.py
+++ experiments/biologic_tests.py
@@ -321,7 +321,7 @@ def run_ocv(potentiostat, channel):
 
 if __name__ == '__main__':
 
-    ip_address = '10.64.0.1'
+    ip_address = '10.64.2.254'
     channel = 7
 
     # Connect to potentiostat
